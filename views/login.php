3806ca27dd2826fc4a1fe9e09e714b269df83202
Jhonatan <jhonatanjau98@gmail.com>
2024-11-21 15:41:04 -0300

    Update

--- views/login.php
+++ views/login.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap"
      rel="stylesheet">
     <link rel="stylesheet" href="css/style-login.css">
+    <link rel="stylesheet" href="css/style-footer.css">
     <link rel="stylesheet" href="css/style-header.css">
 </head>
 <body>
@@ -22,7 +24,7 @@
             <div class="controle">
                 <img class="img-person" src="img/usuario.png">
                 <label for="usuario">
-                    <input class="bx" type="text" id="usuario" placeholder="Digite seu e-mail.." required>
+                    <input class="bx" type="text" id="usuario" placeholder="Digite seu usuario.." required>
                 </label>
             </div>
 
@@ -39,9 +41,9 @@
                 <input class="bxlogin" type="button" id="login" value="Login" required >
             </div>
             
-            <p>Esqueci minha senha</p>
+            <p class="parag">Esqueci minha senha</p>
 
-            <a href="/CCZ/cadastro">
+            <a href="cadastro.php">
                 <div id="reg">Registrar</div>
             </a>
             </div>
@@ -55,4 +57,6 @@
         require_once 'footer.html';
     ?>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </html>
