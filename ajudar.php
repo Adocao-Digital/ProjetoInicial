874b2f52e1fa48d157ef2d92c91919fa77a1d20e
Leo <leopalmeirense90@gmail.com>
2024-12-02 15:29:25 -0300

    update

---
+++ ajudar.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centro de Controle de Zoonoses</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style-ajudar.css">
+    <link rel="stylesheet" href="css/style-header.css">
+    <link rel="stylesheet" href="css/style-footer.css">
+    <link rel="icon" type="image/x-icon" href="img/dogo-argentino.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- Início do cabeçalho -->
+    <?php
+        require_once 'header.html';
+    ?>
+    <!-- Fim do cabeçalho -->
+    <div class="infotexto">
+        <div class="infot">
+            <h1>Como ajudar</h1>
+        </div>
+    </div>
+    <div class="caixa1">
+        <div class="subtitulo">
+            <h5>Ajude com doações</h5>
+        </div>
+        <p>Lorem ipsums iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+    </div>
+
+</body>
+
+<footer>
+    <?php
+        require_once 'footer.html';
+    ?>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</html>
---
+++ quero-adotar.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centro de Controle de Zoonoses</title>
+    <link rel="stylesheet" href="css/style-adotar.css">
+    <link rel="stylesheet" href="css/style-footer.css">    
+    <link rel="stylesheet" href="css/style-header.css">
+    <link rel="icon" type="image/x-icon" href="img/dogo-argentino.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- Início do cabeçalho -->
+    <?php
+        require_once 'header.html';
+    ?>
+    <!-- Fim do cabeçalho -->
+    <div class="infos">
+        <h2>Adote um amigo</h2>
+    </div>
+
+    <div class="corpo">
+        <div class="filtros">
+            <div class="filtro">
+                <h3>Filtros</h3>
+            </div>
+            <div class="filtro">
+                <h3>Espécie</h3>
+            </div>
+            <div class="filtro">
+                <h3>Idade</h3>
+            </div>
+            <div class="filtro">
+                <h3>Porte</h3>    
+            </div>
+            <div class="filtro">
+                <h3>Sexo</h3>    
+            </div>
+        </div>
+
+        <div class="pets">
+            <div class="titulo">
+                <h2>Pets disponíveis</h2>
+            </div>
+            <div class="animais">
+                <div class="linha">
+                    <div class="foto">
+                        <h4>foto</h4>
+                    </div>
+                    <div class="foto">
+                        <h4>foto</h4> 
+                    </div>
+                    <div class="foto">
+                        <h4>foto</h4>
+                    </div>
+                </div>
+                <div class="linha">
+                    <div class="foto">
+                        <h4>foto</h4>
+                    </div>
+                    <div class="foto">
+                        <h4>foto</h4> 
+                    </div>
+                    <div class="foto">
+                        <h4>foto</h4>
+                    </div> 
+                </div>
+            </div>
+
+
+
+
+        </div>
+    </div>
+    <script src="js/script-ativo.js"></script> 
+</body>
+<footer>
+    <?php
+        require_once 'footer.html';
+    ?>
+</footer>
+</html>
+    
