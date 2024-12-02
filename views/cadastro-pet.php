921c6bc061e4124589c29ca1109bf11913132408
Jhonatan <jhonatanjau98@gmail.com>
2024-12-02 08:27:05 -0300

    Update

--- views/cadastro-pet.php
+++ views/cadastro-pet.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Faça seu cadastro</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Cadastre seu pet</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style-pet-1.css">
+    <link rel="stylesheet" href="css/style-footer.css">
     <link rel="stylesheet" href="css/style-header.css">
     <link rel="icon" type="image/x-icon" href="img/dogo-argentino.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -38,7 +39,7 @@
                 <label for="iconfirm">Peso:</label>
                 <input type="password" id="iconfirm" class="estilo-forms" placeholder="Digite o peso atual do seu pet..." required>
                 <div class="cad">
-                    <button type="submit">Cadastrar</button>
+                    <button class="submit-button" type="submit">Cadastrar</button>
                 </div>
             </form>
         </div>
@@ -51,5 +52,7 @@
         require_once 'footer.html';
     ?>
 </footer>
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<script src="js/form.js" defer></script>
 </html>
