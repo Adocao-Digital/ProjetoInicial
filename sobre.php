e5c34972d06a8620ac6d054cf6d90d881b0d8c76
Leonardo Roque <164354205+Leonardo-Roque18@users.noreply.github.com>
2024-12-02 18:56:24 -0300

    Update sobre.php
    upei denovo o mesmo codigo para ver se desbuga

--- sobre.php
+++ sobre.php
@@ -68,8 +68,7 @@
                     </section>
                 </div>
             </div>
-        <!-- </div> -->
-    </div>
+        </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/script-ativo.js"></script>
