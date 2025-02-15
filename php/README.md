586af72ebf015181879d1de7ec362632fbd22343
Kieran Santos <104686090+kieran-santos@users.noreply.github.com>
2025-02-15 12:00:10 -0300

    Delete php directory

--- php/README.md
+++
@@ -1,2 +0,0 @@
-# Projeto Integrador do curso de Desenvolvimento de Software Multiplataforma 01/2024
-### Este projeto apresenta todos os arquivos e documentos de código utilizados para a construção do website do Centro de Controle de Zoonoses de Barra Bonita.
--- php/js/script-ativo.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('nav a');
-    links.forEach(link => {
-        if (link.href === window.location.href) {
-            link.classList.add('active');
-        }
-    });
-});
